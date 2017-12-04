7f8754dfa4702c430952aa82053ab8b541b92871
Evan <evanbyrne89@gmail.com>
2017-12-04 13:58:21 +0000

    Initial commit - First Commit for Mid-point

---
+++ app/src/main/java/com/example/evan/iwallet/VouchersActivity.java
@@ -0,0 +1,13 @@
+package com.example.evan.iwallet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VouchersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vouchers);
+    }
+}
